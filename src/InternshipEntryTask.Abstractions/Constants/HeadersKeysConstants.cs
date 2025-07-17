03e76f25b93b78549b54d9e1d3c11fa01da7282d
Danil Sergeev <poza.voina@gmail.com>
2025-07-17 10:00:57 +0300

    fix

---
+++ src/InternshipEntryTask.Abstractions/Constants/HeadersKeysConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace InternshipEntryTask.Abstractions.Constants;
+
+/// <summary>
+/// Константы ключей заголовков запросов
+/// </summary>
+public class HeadersKeysConstants
+{
+    /// <summary>
+    /// Ключ заголовка управляет отображением доски
+    /// </summary>
+    public const string SHOW_BOARD_KEY = "X-Show-Board";
+
+    /// <summary>
+    /// Ключ заголовка ключ доступа к созданию хода
+    /// </summary>
+    public const string ACCESS_KEY = "X-Access-Key";
+
+    /// <summary>
+    /// Ключ заголовка ключ присоединения к игре
+    /// </summary>
+    public const string JOIN_KEY = "X-Join-Key";
+}
